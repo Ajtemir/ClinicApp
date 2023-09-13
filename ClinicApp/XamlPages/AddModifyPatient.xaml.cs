80e7f32cfa82ecfac276731017e126c95ee6fb64
aulanbekuulu <aulanbekuulu@srs.lan>
2023-09-13 15:20:50 +0600

    Добавил проверку на дату рождения

--- ClinicApp/XamlPages/AddModifyPatient.xaml.cs
+++ ClinicApp/XamlPages/AddModifyPatient.xaml.cs
@@ -73,6 +73,11 @@ namespace ClinicApp.XamlPages
                     status = false;
                     messageBuilder.Append("Дата рождения - обязательное поле для ввода.\n");
                 }
+                else if (dateOfBirth.Value.Date > DateTime.Today)
+                {
+                    status = false;
+                    messageBuilder.Append("Датой рождения выбрана будущая дата.\n");
+                }
                 #endregion
                 #region Checking entered address
                 if (String.IsNullOrEmpty(address))
